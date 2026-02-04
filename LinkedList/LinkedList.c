82d1392ad30e33584ed9899ede4ee7aef7443794
LHofacker <lucashofacker@outlook.com>
2026-02-04 05:27:22 -0300

    Implemented simple 'Insert at end' and 'Print List' for Linked List. Accepting ONLY Int values.

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
+
 struct Node{
 
-	void* data;
+	int data;
 	struct Node* next_node;
 };
 
@@ -21,10 +23,62 @@ struct LinkedList* create_linkedlist(){
 	return temp_list;
 }
 
+void insert(struct LinkedList* list, int input_data){
+
+	struct Node* current_node;
+
+	if(list->head == NULL){
+
+		list->head = malloc(sizeof(struct Node));
+		list->head->data = input_data;
+		list->head->next_node = NULL;
+	}else{
+
+		current_node = list->head;
+
+		while(current_node->next_node != NULL){
+
+			current_node = current_node->next_node;
+		}
+
+		current_node->next_node = malloc(sizeof(struct Node));
+		current_node->next_node->data = input_data;
+		current_node->next_node->next_node = NULL;
+		}
+
+}
+
+void print_list(struct LinkedList* list){
+
+	if(list->head != NULL){
+	
+		struct Node* current_node = list->head;
+
+		while(current_node->next_node != NULL){
+
+			printf("%i\n", current_node->data);
+			current_node = current_node->next_node;
+		}
+
+		printf("%i\n", current_node->data);
+		current_node = current_node->next_node;
+	}else{
+
+		printf("List is empty. Nothing to print\n");
+	}
+
+}
+
 int main(){
 
-	//struct LinkedList* test_list = create_linkedlist();
+	struct LinkedList* test_list = create_linkedlist();
 
+	insert(test_list, 12);
+	insert(test_list, 10);
+	insert(test_list, 34);
+	insert(test_list, 5);
+
+	print_list(test_list);
 
 
 
